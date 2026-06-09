6f4f9e46bda9f202464720f185e283a5472853fc
baoWa2010 <bowa.wang@gmail.com>
2026-06-09 16:15:57 -0700

    Update cordic_main_tb.c
    you now need the header file to run. this way you only need to edit this file if you want to change the test angles.

--- cordic_main_tb.c
+++ cordic_main_tb.c
@@ -1,92 +1,53 @@
 #include <stdio.h>
 #include <math.h>
+#include "cordic_R.h"
 
-int z_table[10] = { 6433, 3798, 2006, 1018, 511, 255, 127, 63, 31, 15 };
+// 1 / CORDIC Gain (A_10) for 10 iterations
+#define CORDIC_GAIN_INV 0.607252935 
 
-void cordic_R_fixed_point( int *x, int *y, int *z);  //defined below
+int main(void) {
+    // Array of angles to test (in degrees)
+    double test_angles[] = {30.0};
+    // only doing one single test for now but should add any edge case scenarios like really small angles
+    int num_tests = sizeof(test_angles) / sizeof(test_angles[0]);
+    
+ for (int t = 0; t < num_tests; t++) {
+        double theta_deg = test_angles[t];
+        double theta_rad = theta_deg * M_PI / 180.0;
 
-/* ------------------------------------------------------------------ 
- verify(): print initial and computed values in both int and float                  
- ------------------------------------------------------------------ */
-void verify( int x_i_init, int y_i_init, int z_i_init,
-             int x_i,      int y_i,      int z_i) {
+        // 1. Initialize fixed-point inputs
+        int x_init = Q_SCALE;                   // x = 1.0 in Q13
+        int y_init = 0;                         // y = 0.0 in Q13
+        int z_init = (int)(theta_rad * Q_SCALE); // target angle in Q13
 
-    double x_d_init, y_d_init, z_d_init, x_d, y_d, z_d;
+        // Variables that will be modified by the CORDIC function
+        int x_i = x_init;
+        int y_i = y_init;
+        int z_i = z_init;
 
-    // convert to floats
-    x_d_init = (double)x_i_init / ( 1 << 13);  
-    y_d_init = (double)y_i_init / ( 1 << 13);  
-    z_d_init = (double)z_i_init / ( 1 << 13);  
+        // 2. Run the CORDIC algorithm (defined in cordic_base.c)
+        cordic_R_fixed_point(&x_i, &y_i, &z_i);
 
-    // Correct for CORDIC gain A[n]: multiply by 1/A[10] = 0.607253  
-    // TODO: might need to make this more concise  
-    x_d = ((double)x_i / ( 1 << 13)) * 0.607253;  
-    y_d = ((double)y_i / ( 1 << 13)) * 0.607253;  
-    z_d = ((double)z_i / ( 1 << 13));             
+        // 3. Convert CORDIC outputs back to float and apply gain correction
+        double cos_cordic = ((double)x_i / Q_SCALE) * CORDIC_GAIN_INV;
+        double sin_cordic = ((double)y_i / Q_SCALE) * CORDIC_GAIN_INV;
+        double z_residual = ((double)z_i / Q_SCALE);
 
-    printf( "x_i_init = %5i\tx_d_init = %f\n", x_i_init, x_d_init);
-    printf( "y_i_init = %5i\ty_d_init = %f\n", y_i_init, y_d_init);
-    printf( "z_i_init = %5i\tz_d_init = %f (rad)\n\n", z_i_init, z_d_init);
+        // 4. Calculate exact math.h values
+        double cos_math = cos(theta_rad);
+        double sin_math = sin(theta_rad);
 
-    printf( "x_i_calc = %5i\tx_d_calc = %f\n", x_i, x_d);
-    printf( "y_i_calc = %5i\ty_d_calc = %f\n", y_i, y_d);
-    printf( "z_i_calc = %5i\tz_d_calc = %f (rad)\n\n", z_i, z_d);
-
-    printf( "cos(z_d_init) = %f\n", cos( z_d_init));
-    printf( "sin(z_d_init) = %f\n", sin( z_d_init));
-
-} // END of verify() function 
-
-/* ------------------------------------------------------------------ 
- cordic_R_fixed_point()                                              
- CORDIC Rotation Mode: drives z -> 0, rotates vector [x, y]                           
- After 10 iterations: x ~ A[n]*cos(z0), y ~ A[n]*sin(z0), z ~ 0    
- ------------------------------------------------------------------ */
-void cordic_R_fixed_point(int *x, int *y, int *z) {
-    int x_temp_1, y_temp_1, z_temp;
-    int x_temp_2, y_temp_2;
-    int i;
-
-    x_temp_1 = *x;
-    y_temp_1 = *y;
-    z_temp   = *z;  //rotation mode: z is an INPUT (target angle)
-
-    for (i = 0; i < 10; i++) {
-        if (z_temp >= 0) {              /* sigma = +1: rotate CCW */
-            x_temp_2 = x_temp_1 - (y_temp_1 >> i);
-            y_temp_2 = y_temp_1 + (x_temp_1 >> i);
-            z_temp  -= z_table[i];
-        } else {                        /* sigma = -1: rotate CW  */
-            x_temp_2 = x_temp_1 + (y_temp_1 >> i);
-            y_temp_2 = y_temp_1 - (x_temp_1 >> i);
-            z_temp  += z_table[i];
-        }
-        x_temp_1 = x_temp_2;
-        y_temp_1 = y_temp_2;
+        // 5. Simplified linear printout
+        printf("--- Test Angle: %.2f deg ---\n", theta_deg);
+        printf("inputs in float:                  angle=%.6f, x=1.000000, y=0.000000\n", theta_rad);
+        printf("inputs to cordic in q13:          z=%d, x=%d, y=%d\n\n", z_init, x_init, y_init);
+        
+        printf("raw cordic output q13:            x=%d, y=%d, z_residual=%d\n", x_i, y_i, z_i);
+        printf("convert to float (gain-adjusted): x=%.6f, y=%.6f, z_residual=%.6f\n\n", cos_cordic, sin_cordic, z_residual);
+        
+        printf("math cos (float):                 %.6f\n", cos_math);
+        printf("math sin (float):                 %.6f\n", sin_math);
+        printf("\n");
     }
-
-    *x = x_temp_1;
-    *y = y_temp_1;
-    *z = z_temp;   // residual angle, should be ~ 0
+    return 0;
 }
-
-// ------------------------------------------------------------------ 
-// main()                                                             
-// ------------------------------------------------------------------
-void main( void) {
-    int x_i_init, y_i_init, z_i_init;  // initial values              
-    int x_i, y_i, z_i;                 
-
-    // Input: unit vector [1, 0] rotated by theta         
-    // x = cos(theta) and y = sin(theta) after gain correction         
-    x_i = (x_i_init = (1 << 13));                          
-    y_i = (y_i_init = 0);                                 
-
-    // Using 30 degrees as initial example 
-    //TODO: test out some more input angles
-    z_i = (z_i_init = (int)(30.0 * M_PI / 180.0 * (1 << 13)));
-
-    cordic_R_fixed_point( &x_i, &y_i, &z_i);
-    verify( x_i_init, y_i_init, z_i_init, x_i, y_i, z_i);
-
-} // END of main() function 
