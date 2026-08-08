6144b184732637af31980f4c0c865c6316a8d25d
mateyab <matey.bsm@gmail.com>
2026-08-08 16:59:47 -0700

    pipeline fix again

--- pipelining/cordic_pipelining.c
+++ pipelining/cordic_pipelining.c
@@ -6,7 +6,6 @@ static const int z_table[10] = { 6433, 3798, 2006, 1018, 511, 255, 127, 63, 31, 
 void cordic_R_fixed_point(int *x, int *y, int *z) {
     int x_temp_1, y_temp_1, z_temp;
     int x_temp_2, y_temp_2;
-    int sign_temp;
     int dz, next_dz;
     int i;
 
@@ -14,18 +13,14 @@ void cordic_R_fixed_point(int *x, int *y, int *z) {
     y_temp_1 = *y;
     z_temp   = *z;
 
-    // Extract initial decision to pipeline condition evaluation
-    sign_temp = (z_temp >= 0);
-
     // Preload angle for iteration 0 before loop starts
     next_dz = z_table[0];
-
 
     for (i = 0; i < 9; i++) {
         dz = next_dz;                 // angle loaded ahead of time
         next_dz = z_table[i + 1];     // preload next iteration's angle now
 
-        if (sign_temp) {
+        if (z_temp >= 0) {
             x_temp_2 = x_temp_1 - (y_temp_1 >> i);
             y_temp_2 = y_temp_1 + (x_temp_1 >> i);
             z_temp  -= dz;
@@ -35,8 +30,6 @@ void cordic_R_fixed_point(int *x, int *y, int *z) {
             z_temp  += dz;
         }
 
-        sign_temp = (z_temp >= 0);
-
         x_temp_1 = x_temp_2;
         y_temp_1 = y_temp_2;
     }
@@ -44,7 +37,7 @@ void cordic_R_fixed_point(int *x, int *y, int *z) {
     // last iteration
     dz = next_dz; 
 
-    if (sign_temp) {
+    if (z_temp >= 0) {
         x_temp_2 = x_temp_1 - (y_temp_1 >> 9);
         y_temp_2 = y_temp_1 + (x_temp_1 >> 9);
         z_temp  -= dz;
