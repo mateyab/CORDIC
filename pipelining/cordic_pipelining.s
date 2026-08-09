fb6ca844552d8d53e621b4f2603d5636180b6c99
mateyab <matey.bsm@gmail.com>
2026-08-08 17:04:14 -0700

    update pipeline

--- pipelining/cordic_pipelining.s
+++ pipelining/cordic_pipelining.s
@@ -22,62 +22,52 @@
 cordic_R_fixed_point:
 	@ args = 0, pretend = 0, frame = 0
 	@ frame_needed = 0, uses_anonymous_args = 0
-	push	{r4, r5, r6, r7, r8, r9, r10, lr}
+	push	{r4, r5, r6, r7, r8, r9, lr}
 	mov	ip, #0
-	ldr	r5, [r2]
-	ldr	r8, .L12
-	movw	lr, #6433
-	mvns	r4, r5
-	ldr	r7, [r0]
-	ldr	r6, [r1]
+	ldr	lr, .L10
+	ldr	r6, [r0]
+	movw	r7, #6433
+	ldr	r5, [r1]
 .LPIC0:
-	add	r8, pc
-	lsrs	r4, r4, #31
-	b	.L4
-.L11:
-	sub	r5, r5, r9
-	sub	r7, r7, r10
-	mvns	r4, r5
-	add	r6, r6, r3
-	cmp	ip, #9
-	lsr	r4, r4, #31
-	beq	.L10
+	add	lr, pc
+	ldr	r4, [r2]
 .L4:
 	mov	r3, ip
-	mov	r9, lr
-	asr	r10, r6, ip
-	ldr	lr, [r8, #4]!
-	add	ip, ip, #1
-	asr	r3, r7, r3
 	cmp	r4, #0
-	bne	.L11
-	add	r5, r5, r9
-	add	r7, r7, r10
-	mvns	r4, r5
-	subs	r6, r6, r3
-	cmp	ip, #9
-	lsr	r4, r4, #31
-	bne	.L4
-.L10:
-	cmp	r5, #0
-	asr	r4, r7, #9
-	asr	r3, r6, #9
+	asr	r9, r5, ip
+	mov	r8, r7
+	asr	r3, r6, r3
+	add	ip, ip, #1
 	ittte	ge
-	addge	r4, r4, r6
-	subge	r7, r7, r3
-	subge	r5, r5, #15
-	addlt	r7, r7, r3
+	subge	r6, r6, r9
+	addge	r5, r5, r3
+	subge	r4, r4, r8
+	addlt	r6, r6, r9
+	itt	lt
+	sublt	r5, r5, r3
+	addlt	r4, r4, r8
+	ldr	r7, [lr, #4]!
+	cmp	ip, #9
+	bne	.L4
+	cmp	r4, #0
+	asr	r3, r5, #9
+	asr	r7, r6, #9
+	ittte	ge
+	subge	r3, r6, r3
+	addge	r5, r5, r7
+	subge	r4, r4, #15
+	addlt	r3, r3, r6
 	it	lt
-	sublt	r4, r6, r4
-	str	r7, [r0]
+	sublt	r5, r5, r7
+	str	r3, [r0]
 	it	lt
-	addlt	r5, r5, #15
-	str	r4, [r1]
-	str	r5, [r2]
-	pop	{r4, r5, r6, r7, r8, r9, r10, pc}
-.L13:
+	addlt	r4, r4, #15
+	str	r5, [r1]
+	str	r4, [r2]
+	pop	{r4, r5, r6, r7, r8, r9, pc}
+.L11:
 	.align	2
-.L12:
+.L10:
 	.word	.LANCHOR0-(.LPIC0+4)
 	.size	cordic_R_fixed_point, .-cordic_R_fixed_point
 	.section	.rodata
